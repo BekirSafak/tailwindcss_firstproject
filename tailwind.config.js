c9e08668105241608a118f1892a70c31a3021fca
Bekir ŞAFAK <bekir_safak_38@hotmail.com>
2023-06-21 18:48:53 +0300

    Tailwind custom fonts ve colors ayarları yapıldı.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    container: {
+      // Burada container yazılan her yerin kendisini ortalaması sağlandı.
+      center: true,
+    },
+
+    extend: {
+      fontFamily: {
+        // Burada custom font eklendi.
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif']
+      },
+
+      colors: {
+        // Burada custom color eklendi.
+        'flower-yellow': '#FFE873',
+        'flower-green': '#91A561',
+        'flower-pink': '#E4C2D2',
+        'flower-blue': '#3F6E9E',
+        'flower-light-blue': '#97BEC9',
+        'flower-light-green': '#C1E39E'
+
+
+      }
+    },
   },
   plugins: [],
 }
