9ab11337187d95d99886cb3e8150d910e67ad8ed
Bekir ŞAFAK <bekir_safak_38@hotmail.com>
2023-06-26 22:39:48 +0300

    tailwind darkmode eklendi.

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,23 @@ hamburgerBTN.addEventListener("click", function openMenu() {
     dropMenu.classList.toggle('hidden');
 })
 // DropMenu End
+
+// DarkMode Start
+let sunBTN = document.querySelector('.sunBTN');
+let moonBTN = document.querySelector('.moonBTN');
+let body = document.querySelector('body');
+
+console.log(body)
+
+sunBTN.addEventListener("click", function sun() {
+    sunBTN.classList.toggle('hidden');
+    moonBTN.classList.toggle('hidden');
+    body.classList.remove('dark');
+})
+
+moonBTN.addEventListener("click", function moon() {
+    sunBTN.classList.toggle('hidden');
+    moonBTN.classList.toggle('hidden')
+    body.classList.add('dark');
+})
+// DarkMode End
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  // Burada darkmode özelliği eklendi.
+  darkMode: 'class',
   theme: {
 
     container: {
